2575656864e852cfb91d7e0c2e964a3a817b686d
dedehsantos <dedehfrontend@gmail.com>
2020-07-30 01:25:11 -0300

    treino

---
+++ _js/script.js
@@ -0,0 +1,12 @@
+// velocidade maxima de ate 70
+//  acada 5km acima do  limite você perde 1 ponto
+// math.Florar()
+// Caso pontos maior que 12 -> "Carteira Suspendida"
+
+var botao = document.getElementById("btn");
+btn.addEventListener("mouseover", function(){
+    document.getElementsByClassName('personalizado')
+
+
+    
+})
